200a55c48db66f5b32fbbb2804381dae4dc5bb2b
Jake <github@jakewendt.com>
2016-10-26 12:49:52 -0700

    Deving eligibility screenings.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ end
 class ActionView::Helpers::FormBuilder
 	def radio_button_with_label(method, tag_value, options = {})
 		@template.label( @object_name, method, tag_value, 
-				objectify_options(options.merge(:class => 'with_radio_button')) ) do
+				objectify_options(options.merge(:class => 'with_radio_button', :value => tag_value )) ) do
 			@template.radio_button( @object_name, 
 				method, tag_value, objectify_options(options) ) << tag_value
 		end
