85f30a7e9eb183b81cb1c695629c10eac6dc2bee
Jake <github@jakewendt.com>
2016-11-02 14:18:23 -0700

    Added demographics counting.

--- app/controllers/statics_controller.rb
+++ app/controllers/statics_controller.rb
@@ -1,2 +1,14 @@
 class StaticsController < ApplicationController
+
+
+#	Question.where(:title => "RACE")
+#	GENDER
+#	HISPLAT
+
+	def counts
+		studyId = 1
+		@races = Interview.races
+		@demographics = Study.find(studyId).interviews.collect(&:demographics)
+	end
+
 end
--- app/models/interview.rb
+++ app/models/interview.rb
@@ -1,4 +1,38 @@
 class Interview < ApplicationRecord
 	self.table_name = "interview"
 	belongs_to :study, foreign_key: :studyId
+	has_many :answers, foreign_key: :interviewId
+	has_many :questions, through: :answers
+
+	def self.races
+		races = Question.where(:title => "RACE").collect(&:question_options).flatten.collect(&:name).sort
+		#	Race is in English and Spanish. Take just the English.
+		races = races.collect{|race|race[0..race.index("/")-1]}
+		races << "More Than One Race"
+		races << "Unknown or Not Reported"
+	end
+
+	def demographics
+		{ :race => race, :gender => gender, :hispanic => hispanic }
+	end
+
+	def race
+		( races.length <= 0 ) ? ["Unknown or Not Reported"] : 
+			( races.length > 1 ) ? ["More Than One Race"] : races
+	end
+
+	def races
+		races = answers.joins(:question).where('question.title' => "RACE").collect(&:option_names).flatten
+		#	Race is in English and Spanish. Take just the English.
+		races.collect{|race|race[0..race.index("/")-1]}
+	end
+
+	def gender
+		answers.joins(:question).where('question.title' => "GENDER").collect(&:option_names).flatten
+	end
+
+	def hispanic
+		answers.joins(:question).where('question.title' => "HISPLAT").collect(&:option_names).flatten
+	end
+
 end
