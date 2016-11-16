558d1e9e84b08a9e2ff4c85783414e93c93fa123
Jake <github@jakewendt.com>
2016-11-16 13:50:04 -0800

    Cleanup

--- app/models/rails_user.rb
+++ app/models/rails_user.rb
@@ -1,11 +1,11 @@
 class RailsUser < ApplicationRecord
-#class User < ActiveRecord::Base
 	acts_as_authentic do |c|
 #		c.my_config_option = my_value
 #		c.session_class = RailsUserSession
 	end # the configuration block is optional
 end
 
-
+#	EGOWeb seems to lock the database so can't even connect?
 
 #	RailsUser.create(:login => 'jake', :password => 'password', :password_confirmation => 'password')
+
