437fcaa923fc25f26a32cc32dd6570e564d6ab1c
Jake <github@jakewendt.com>
2016-11-28 15:57:27 -0800

    Added 2 questions and converted string values to simple integers.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,12 @@
 module ApplicationHelper
 end
 class ActionView::Helpers::FormBuilder
-	def radio_button_with_label(method, tag_value, options = {})
-		@template.label( @object_name, method, tag_value, 
-				objectify_options(options.merge(:class => 'with_radio_button', :value => tag_value )) ) do
+	def radio_button_with_label(method, label, value = nil, options = {})
+		value = label if value.blank?
+		@template.label( @object_name, method, value, 
+				objectify_options(options.merge(:class => 'with_radio_button', :value => value )) ) do
 			@template.radio_button( @object_name, 
-				method, tag_value, objectify_options(options) ) << tag_value
+				method, value, objectify_options(options) ) << label
 		end
 	end
 end
--- db/migrate/20161116214940_create_testing_facilitations.rb
+++ db/migrate/20161116214940_create_testing_facilitations.rb
@@ -4,22 +4,24 @@ class CreateTestingFacilitations < ActiveRecord::Migration[5.0]
 			t.date :date_of_visit
 			t.string :participant_id
 			t.string :staff_person
-			t.string :agree_to_use_home_test_kit_at_site, limit: 10
+			t.integer :agree_to_use_home_test_kit_at_site
 			t.string :agree_to_use_home_test_kit_at_site_refusal_reason
-			t.string :agree_to_take_test_kit_home, limit: 10
+			t.integer :agree_to_take_test_kit_home
 			t.string :agree_to_take_test_kit_home_refusal_reason
-			t.string :agree_to_tell_result_of_home_test_kit, limit: 10
+			t.integer :agree_to_tell_result_of_home_test_kit
 			t.string :agree_to_tell_result_of_home_test_kit_refusal_reason
-			t.string :result_of_home_test_kit, limit: 25
-			t.string :confirmatory_test_referred_location
+			t.integer :result_of_home_test_kit
+			t.integer :confirmatory_test_referred_location
 			t.datetime :confirmatory_test_referred_appointment
 			t.string :confirmatory_test_referred_location_other
-			t.string :indeterminate_test_option, limit: 25
+			t.integer :indeterminate_test_option
 			t.string :indeterminate_test_result
 			t.string :indeterminate_test_referred_to
-			t.string :urine_to_test, limit: 10
-			t.string :urine_to_test_refusal_reason
+			t.integer :urine_to_test
+			t.integer :urine_to_test_refusal_reason
 			t.string :urine_to_test_refusal_reason_other
+			t.integer :gonorrhea_result
+			t.integer :chlamydia_result
 			t.text :notes
 
 			t.timestamps
