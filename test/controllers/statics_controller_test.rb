5f02498398d1a66774692d83593bdd3ca1265b3d
Jake <github@jakewendt.com>
2017-02-03 16:25:48 -0800

    Noting the need for some tests.

--- test/controllers/statics_controller_test.rb
+++ test/controllers/statics_controller_test.rb
@@ -4,15 +4,87 @@ class StaticsControllerTest < ActionDispatch::IntegrationTest
 
 	%w{admin nonadmin}.each do |login|
 
-		test "should not get index with #{login} login" do
+		test "should get index with #{login} login" do
 			create_and_login_as(login)
 			get statics_url
 			assert_response :success
 		end
 
-		test "should not get reports with #{login} login" do
+		test "should get reports with #{login} login" do
 			create_and_login_as(login)
 			get reports_statics_url
+			assert_response :success
+		end
+
+		test "should get black_demographic_counts with #{login} login" do
+
+#	TODO create some demographics to count AFTER model tests!
+
+			create_and_login_as(login)
+			get black_demographic_counts_statics_url
+			assert_response :success
+		end
+
+		test "should get latina_demographic_counts with #{login} login" do
+
+#	TODO create some demographics to count AFTER model tests!
+
+			create_and_login_as(login)
+			get latina_demographic_counts_statics_url
+			assert_response :success
+		end
+
+		test "should get trans_demographic_counts with #{login} login" do
+
+#	TODO create some demographics to count AFTER model tests!
+
+			create_and_login_as(login)
+			get trans_demographic_counts_statics_url
+			assert_response :success
+		end
+
+		test "should get demographic_counts with #{login} login" do
+
+#	TODO create some demographics to count AFTER model tests!
+
+			create_and_login_as(login)
+			get demographic_counts_statics_url
+			assert_response :success
+		end
+
+		test "should get leftover_demographic_counts with #{login} login" do
+
+#	TODO create some demographics to count AFTER model tests!
+
+			create_and_login_as(login)
+			get leftover_demographic_counts_statics_url
+			assert_response :success
+		end
+
+		test "should get sti_counts with #{login} login" do
+
+#	TODO create some data to count AFTER model tests!
+
+			create_and_login_as(login)
+			get sti_counts_statics_url
+			assert_response :success
+		end
+
+		test "should get alters_per_ego with #{login} login" do
+
+#	TODO create some data to count AFTER model tests!
+
+			create_and_login_as(login)
+			get alters_per_ego_statics_url
+			assert_response :success
+		end
+
+		test "should get screening_report_eligible with #{login} login" do
+
+#	TODO create some data to count AFTER model tests!
+
+			create_and_login_as(login)
+			get screening_report_eligible_statics_url
 			assert_response :success
 		end
 
