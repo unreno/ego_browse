7e0803a17ffa659031003752bcea276944576109
Jake <github@jakewendt.com>
2017-02-15 10:45:40 -0800

    Disconnected radio button from label.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,8 @@ end
 class ActionView::Helpers::FormBuilder
 	def radio_button_with_label(method, label, value = nil, options = {})
 		value = label if value.blank?
-		@template.label( @object_name, method, value, 
+#		@template.label( @object_name, method, "#{value}_disconnect", 
+		@template.label( @object_name, "#{method}_disconnected",
 				objectify_options(options.merge(:class => 'with_radio_button', :value => value )) ) do
 			@template.radio_button( @object_name, 
 				method, value, objectify_options(options) ) << label
