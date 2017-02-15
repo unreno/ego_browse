2845b87c1068ee485eacf5748dda74654d1ed585
Jake <github@jakewendt.com>
2017-02-15 14:21:50 -0800

    Cleaning up radio buttons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,10 +2,6 @@ module ApplicationHelper
 end
 class ActionView::Helpers::FormBuilder
 	def clear_all_radio_button_with_label(method, label=nil, value = nil, options = {})
-#		value = label if value.blank?
-#		@template.label( @object_name, method, "#{value}_disconnect", 
-#		@template.label( @object_name, "#{method}_disconnected",
-#				objectify_options(options.merge(:class => 'with_radio_button', :value => value )) ) do
 		@template.label( @object_name, "#{method}_disconnected" ) do
 			@template.radio_button( @object_name, 
 				method, '', objectify_options(options) ) << 'clear all'
@@ -13,13 +9,11 @@ class ActionView::Helpers::FormBuilder
 	end
 	def radio_button_with_label(method, label, value = nil, options = {})
 		value = label if value.blank?
-#		@template.label( @object_name, method, "#{value}_disconnect", 
-#		@template.label( @object_name, "#{method}_disconnected",
-#				objectify_options(options.merge(:class => 'with_radio_button', :value => value )) ) do
-		@template.label( @object_name, "#{method}_disconnected",
-				objectify_options(options.merge( :value => value )) ) do
+		@template.content_tag( :div, class: 'field' )  do
 			@template.radio_button( @object_name, 
-				method, value, objectify_options(options) ) << label
+				method, value, objectify_options(options) ) << 
+			@template.label( @object_name, method, label,
+				objectify_options(options.merge( :value => value )) )
 		end
 	end
 end
