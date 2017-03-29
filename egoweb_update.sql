ec0fde2c95f97e658b65357149429069410db691
Jake <github@jakewendt.com>
2017-03-29 13:37:54 -0700

    A few columns were added to the database and they are required in some methods so we need to add them.

---
+++ egoweb_update.sql
@@ -0,0 +1,6 @@
+
+ALTER TABLE matchedAlters ADD interviewId1 INT(11);
+ALTER TABLE matchedAlters ADD interviewId2 INT(11);
+ALTER TABLE matchedAlters ADD userId INT(11);
+ALTER TABLE study ADD header LONGTEXT;
+
