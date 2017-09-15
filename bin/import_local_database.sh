78862d9906e43b1161473d76c3734c2506881140
Jake <github@jakewendt.com>
2017-09-15 12:51:04 -0700

    Deving import database script

---
+++ bin/import_local_database.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+
+
+#	Save your database credentials so you won't be prompted for them
+#
+#	vi ~/.my.cnf
+#	* add mysql credentials
+#
+#		[mysql]
+#		database=egoweb
+#		user=ruby
+#		password=
+#
+#	chmod 400 ~/.my.cnf
+#
+
+
+mount box
+cp "$( ls -1tr box/DOTS\ Global/Data/dbdumps/*.egoweb.sql.gz | tail -n 1 )" ~/dbdumps/
+umount box
+mysql -u ruby egoweb < <(zcat $(ls -1tr ~/dbdumps/*.egoweb.sql.gz | tail -n 1 ) )
+
+#	update database to match latest version of egoweb requirements
+#
+#	mysql -u ruby egoweb < /var/www/ego_browse/egoweb_update.sql
+
+
